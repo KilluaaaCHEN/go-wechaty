15350f05916c3d4c5c87c2d3a5f10629c3e841ee
chenzhou <zhou.chen@yo-star.com>
2021-06-09 18:08:58 +0800

    fixed

--- Update.sh
+++ Update.sh
@@ -1,7 +1,7 @@
 git pull
 name="go-wechaty"
 version="1.0"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./runtime/main ./main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -n -o ./runtime/main ./main.go
 docker build -t "${name}:${version}" -f "./Dockerfile" .
 docker stop ${name}
 docker rm ${name}
